7d6233dd72ade71b4b891ba1383a4eac3c15c533
David García Garzón <david.garcia@somenergia.coop>
2024-01-27 10:34:00 +0100

    ✨ DiceRollerControl as Control

---
+++ scenes/dice_roller/dice_roller_control.gd
@@ -0,0 +1,14 @@
+## A Control holding an actionable Dice Roller
+@icon("res://tiles/dice_roller_controller.svg")
+extends SubViewportContainer
+class_name DiceRollerControl
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
